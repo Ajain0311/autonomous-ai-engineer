73e6665094f39637150562ca97f4cc3b1ebe6bf7
Ajain0311 <Ajain0311@users.noreply.github.com>
2026-08-01 16:00:37 +0530

    feat(tech-hub): initialize directories and configuration

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="container mx-auto p-4 pt-6 mt-10">
+      <h1 className="text-3xl font-bold">Tech Hub</h1>
+    </div>
+  );
+}
+export default App;
